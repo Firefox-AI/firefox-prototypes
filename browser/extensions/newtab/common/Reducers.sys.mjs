6b23a5ca258f75934f89155f44efb60fa6c4932e
Brian Grinstead <briangrinstead@gmail.com>
2025-09-12 14:10:44 -0700

    Use a different page for new tab + sidebar

--- browser/extensions/newtab/common/Reducers.sys.mjs
+++ browser/extensions/newtab/common/Reducers.sys.mjs
@@ -188,10 +188,6 @@ export const INITIAL_STATE = {
       },
     },
   },
-  // Smart Window state
-  SmartWindow: {
-    active: false,
-  },
   TimerWidget: {
     // The timer will have 2 types of states, focus and break.
     // Focus will the default state
@@ -1198,15 +1194,6 @@ function ListsWidget(prevState = INITIAL_STATE.ListsWidget, action) {
   }
 }
 
-function SmartWindow(prevState = INITIAL_STATE.SmartWindow, action) {
-  switch (action.type) {
-    case "SMART_WINDOW_STATE_UPDATE":
-      return { ...prevState, active: action.data.smartWindowActive };
-    default:
-      return prevState;
-  }
-}
-
 export const reducers = {
   TopSites,
   App,
@@ -1226,5 +1213,4 @@ export const reducers = {
   TrendingSearch,
   Wallpapers,
   Weather,
-  SmartWindow,
 };
