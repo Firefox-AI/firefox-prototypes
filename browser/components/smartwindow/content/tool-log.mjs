790aa1495426936a4c0731d2f21384598a1974db
Ed Lee <edilee@mozilla.com>
2025-10-17 12:53:27 -0700

    lint --fix smartwindow/content

--- browser/components/smartwindow/content/tool-log.mjs
+++ browser/components/smartwindow/content/tool-log.mjs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import { html, css } from "chrome://global/content/vendor/lit.all.mjs";
 import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 
