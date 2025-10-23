ec99ec0c9d8c41dc8d7303657b11c88e93bab279
punamdahiya <punamdahiya@yahoo.com>
2025-10-23 03:46:37 -0700

    Full page welcome screen with terms of service (#28)
    * Full page welcome screen with terms of service before smart window switch
    
    * dedicated dev onboarding menu entry
    
    * lint --fix
    
    ---------
    
    Co-authored-by: Punam Dahiya <pdahiya@mozilla.com>
    Co-authored-by: Ed Lee <edilee@mozilla.com>

---
+++ browser/components/smartwindow/content/welcome.js
@@ -0,0 +1,163 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const lazy = {};
+const { topChromeWindow } = window.browsingContext;
+const { XPCOMUtils } = ChromeUtils.importESModule(
+  "resource://gre/modules/XPCOMUtils.sys.mjs"
+);
+
+ChromeUtils.defineESModuleGetters(lazy, {
+  AboutWelcomeParent: "resource:///actors/AboutWelcomeParent.sys.mjs",
+});
+
+XPCOMUtils.defineLazyPreferenceGetter(
+  lazy,
+  "onboardingConfig",
+  "browser.smartwindow.onboarding.config",
+  JSON.stringify({
+    id: "smart-window-welcome",
+    template: "multistage",
+    transitions: true,
+    screens: [
+      {
+        id: "welcome_screen",
+        content: {
+          fullscreen: true,
+          hide_secondary_section: "responsive",
+          narrow: true,
+          position: "split",
+
+          title: {
+            fontWeight: 600,
+            fontSize: "55px",
+            letterSpacing: "revert",
+            raw: "Meet Smart Window a new way to browse",
+          },
+          title_style: "fancy shine",
+          text_color: "dark",
+          subtitle: {
+            fontWeight: 400,
+            raw: "Get answers when you need them. Keep your tabs tiday. Enjoy suggestions that feel made just for you. And because this is Firefox, your privacy always comes first.",
+          },
+
+          cta_paragraph: {
+            text: {
+              string_id: "genai-welcome-onboarding-tos",
+            },
+            action: {
+              navigate: true,
+            },
+          },
+          primary_button: {
+            label: "Get Started",
+            action: {
+              type: "MULTI_ACTION",
+              dismiss: true,
+              data: {
+                actions: [
+                  {
+                    type: "SET_PREF",
+                    data: {
+                      pref: {
+                        name: "messaging-system-action.smart-window-tos",
+                        value: true,
+                      },
+                    },
+                  },
+                  {
+                    data: {
+                      entrypoint: "activity-stream-firstrun",
+                    },
+                    type: "SHOW_FIREFOX_ACCOUNTS",
+                  },
+                ],
+              },
+            },
+          },
+          additional_button: {
+            label: "Learn More",
+            style: "link",
+            action: {
+              data: {
+                args: "https://support.mozilla.org/",
+                where: "tabshifted",
+              },
+              type: "OPEN_URL",
+            },
+          },
+        },
+      },
+    ],
+  })
+);
+
+function addStylesheet(href) {
+  const link = document.head.appendChild(document.createElement("link"));
+  link.rel = "stylesheet";
+  link.href = href;
+}
+
+// TODO
+// Check if a) is signedIn b)  Has FxAccount and not signed in  c) doesn't have FxAccount
+// a) Check consent stored in pref `messaging-system-action.smart-window-tos` , if yes toggle to smart window
+// b) and c) Redirect to account.firefox.com for account creation and signin and on success call toggle smart window
+
+/**
+ * Render content based on about:welcome multistage template.
+ *
+ * @param ready
+ */
+function renderMultistage(ready) {
+  const AWParent = new lazy.AboutWelcomeParent();
+  const receive = name => data =>
+    AWParent.onContentMessage(
+      `AWPage:${name}`,
+      data,
+      topChromeWindow.gBrowser.selectedBrowser
+    );
+
+  // Expose top level functions expected by the bundle.
+  window.AWGetFeatureConfig = () => JSON.parse(lazy.onboardingConfig);
+  window.AWGetSelectedTheme = receive("GET_SELECTED_THEME");
+  window.AWGetInstalledAddons = receive("GET_INSTALLED_ADDONS");
+  window.AWSelectTheme = data => receive("SELECT_THEME")(data?.toUpperCase());
+  window.AWSendEventTelemetry = receive("TELEMETRY_EVENT");
+
+  window.AWSendToDeviceEmailsSupported = receive(
+    "SEND_TO_DEVICE_EMAILS_SUPPORTED"
+  );
+  window.AWAddScreenImpression = receive("ADD_SCREEN_IMPRESSION");
+  window.AWSendToParent = (name, data) => receive(name)(data);
+  window.AWFinish = () => {};
+  window.AWWaitForMigrationClose = receive("WAIT_FOR_MIGRATION_CLOSE");
+  window.AWEvaluateScreenTargeting = receive("EVALUATE_SCREEN_TARGETING");
+  window.AWEvaluateAttributeTargeting = receive("EVALUATE_ATTRIBUTE_TARGETING");
+
+  // Update styling to be compatible with about:welcome.
+  addStylesheet("chrome://browser/content/aboutwelcome/aboutwelcome.css");
+
+  document.body.classList.add("onboardingContainer");
+  document.body.id = "multi-stage-message-root";
+  // This value is reported as the "page" in telemetry
+  document.body.dataset.page = "smart-window-welcome";
+  const bundleScript = document.head.appendChild(
+    document.createElement("script")
+  );
+  bundleScript.src =
+    "chrome://browser/content/aboutwelcome/aboutwelcome.bundle.js";
+
+  ready();
+}
+
+// Initialize when DOM is ready
+if (document.readyState === "loading") {
+  document.addEventListener(
+    "DOMContentLoaded",
+    () => renderMultistage(() => {}),
+    { once: true }
+  );
+} else {
+  renderMultistage(() => {});
+}
